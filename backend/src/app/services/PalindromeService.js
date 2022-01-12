edc163cd0a59e09f1a61d6d5591b632c4de231ff
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2022-01-12 10:48:21 -0300

    feat: palindrome

---
+++ backend/src/app/services/PalindromeService.js
@@ -0,0 +1,17 @@
+const isPalindrome = (number) => {
+  const reversedNumber = number.toString().split('').reverse().join('');
+  return reversedNumber == number;
+};
+
+const palindromeList = (initial, final) => {
+  const valuesList = [];
+  for (let index = initial; index <= final; index += 1) {
+    if (isPalindrome(index)) {
+      valuesList.push(index);
+    }
+  }
+  return valuesList;
+};
+
+
+export { palindromeList };
