614417bd95dfbce5b0926109c183209c70c09c8e
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2022-01-12 20:44:45 -0300

    feat: car controller and routes

--- backend/src/app/controllers/CarController.js
+++ backend/src/app/controllers/CarController.js
@@ -1,6 +1,38 @@
-import { Car } from '../services/CarService.ts';
+const { Car } = require('../services/CarService.ts');
+const { DATABASE } = require('../../mocks/database.js');
+
 
 class CarController {
+  store(request, response) {
+    const { model, year, brand, doors } = request.body;
+
+    if (!model || !year || !brand || !doors) {
+      return response.status(400).json({ error: 'É preciso preencher todos os campos' });
+    }
+
+    if (typeof model !== 'string' || typeof brand !== 'string') {
+      return response.status(400).json({ error: 'Os campos model e brand devem ser do tipo string' });
+    }
+
+    if (typeof year !== 'number' || typeof doors !== 'number') {
+      return response.status(400).json({ error: 'Os campos year e doors devem ser do tipo number' });
+    }
+
+    if (![2, 4].includes(doors)) {
+      return response.status(400).json({ error: 'Só é possível escolher 2 ou 4 portas' });
+    }
+
+    DATABASE.cars.push(new Car(
+      model,
+      year,
+      brand,
+      doors
+    ));
+    return response.json(DATABASE.cars);
+  }
+  show(request, response) {
+    return response.json(DATABASE.cars);
+  }
 
 }
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const { Router } = require('express');
 const PalindromeController = require('./app/controllers/PalindromeController.js');
 const PurchaseController = require('./app/controllers/PurchaseController.js');
 const MotocycleController = require('./app/controllers/MotocycleController.js');
+const CarController = require('./app/controllers/CarController.js');
 
 
 const router = Router();
@@ -11,5 +12,7 @@ router.post('/palindrome', PalindromeController.show);
 router.post('/purchase', PurchaseController.show);
 router.post('/motocycle', MotocycleController.store);
 router.get('/motocycle', MotocycleController.show);
+router.post('/car', CarController.store);
+router.get('/car', CarController.show);
 
 module.exports = router;
