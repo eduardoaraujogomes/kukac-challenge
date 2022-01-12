a9a6c82b71fb69e2bd7374f89ecf2d6eedf7fe5c
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2022-01-12 12:03:16 -0300

    feat: purchase

---
+++ backend/src/app/services/PurchaseService.js
@@ -0,0 +1,28 @@
+
+const rest = (value, note) => value % note;
+
+
+const changePurchase = (purchaseValue, payment) => {
+  const noteChange = (note, value) => {
+    if (change - rest(note, value) === 0) {
+      return 0;
+    }
+    const notes = (change - rest(value, note)) / note;
+    change = change - (notes * note);
+    return notes;
+  };
+  let change = payment - purchaseValue;
+
+
+  return {
+    change,
+    'notes of 100': noteChange(100, change),
+    'notes of 10': noteChange(10, change),
+    'notes of 1': noteChange(1, change)
+  };
+
+};
+
+
+
+export { changePurchase };
